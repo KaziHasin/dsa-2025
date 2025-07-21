2b5cefdb1008cb9437b1415efc713ddafa90bf60
Kazi Hasin <akashkazi012@gmail.com>
2025-07-21 10:45:20 +0530

    Add bubble sort implementation with array printing functionality

---
+++ sorting/bubble_sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void swapArray(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void printArray(int arr[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    int isSortted;
+    for (i = 0; i < n - 1; i++)
+    {
+        printf("Number of pass %d: \n", i + 1);
+        isSortted = 0;
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swapArray(&arr[j], &arr[j + 1]);
+                isSortted = 1;
+            }
+        }
+        if (!isSortted)
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 4, 5, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before sorting the array: \n");
+    printArray(arr, size);
+
+    bubbleSort(arr, size);
+
+    printf("After sorting the array: \n");
+    printArray(arr, size);
+    return 0;
+}
